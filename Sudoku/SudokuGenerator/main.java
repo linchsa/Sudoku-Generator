a77af0ecf57ef8f2d82ed124c730314815757575
CelerX <54667438+linchsa@users.noreply.github.com>
2022-02-05 00:30:30 +0100

    Delete Sudoku/SudokuGenerator directory

--- Sudoku/SudokuGenerator/main.java
+++
@@ -1,9 +0,0 @@
-package SudokuGenerator;
-class Sudoku {
-   public static void main(String[] args){
-       int sudoku[][] = new int [9][9];
-       startSudoku.initialize(sudoku);
-       int sudokufinal[][]= check.generate(sudoku);
-       startSudoku.print(sudokufinal);
-   }//end of main
-}// end of class
